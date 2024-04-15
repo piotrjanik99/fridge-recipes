95f21d5d0d7ac90dd02c7045e6027e93a2c7bbcc
Piotr <azaer99@gmail.com>
2024-04-15 17:29:09 +0200

    added tailwind css

--- fridge-recipes/screens/HomeScreen.js
+++ fridge-recipes/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView className="flex-1 justify-center items-center bg-white">
+      <Text className="text-black text-3xl font-bold">HomeScreen</Text>
+      <StatusBar style="dark" />
     </SafeAreaView>
   )
 }
