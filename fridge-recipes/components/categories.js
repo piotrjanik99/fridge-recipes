f00c33c3616b4f46def1365cfa6a6264c586a041
Piotr <azaer99@gmail.com>
2024-04-17 19:32:35 +0200

    added categories, api connection and searchbar

---
+++ fridge-recipes/components/categories.js
@@ -0,0 +1,41 @@
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import {categoryData} from '../constans'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+
+export default function Categories({categories, activeCategory, setActiveCategory}) {
+  return (
+    <Animated.View entering={FadeInDown.duration(500).springify()}>
+      <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="space-x-4"
+      contentContainerStyle={{paddingHorizontal: 15}}>
+        {
+            categories.map((cat, index) => {
+                let isActive = cat.strCategory == activeCategory
+                let activeButtonClass = isActive ? 'bg-sky-300' : 'bg-black/10'
+                return (
+                    <TouchableOpacity
+                    key={index}
+                    onPress={() => setActiveCategory(cat.strCategory)}
+                    className="flex items-center space-y-1">
+                        <View className={"rounded-full p-[6px] "+activeButtonClass}>
+                            <Image
+                                source={{uri: cat.strCategoryThumb}}
+                                style={{width: hp(6), height: hp(6)}}
+                                className="rounded-full">
+                            </Image>
+                        </View>
+                        <Text className="text-neutral-600" style={{fontSize: hp(1.6)}}>
+                            {cat.strCategory}
+                        </Text>
+                    </TouchableOpacity>
+                )
+            })
+        }
+      </ScrollView>
+    </Animated.View>
+  )
+}
--- fridge-recipes/screens/HomeScreen.js
+++ fridge-recipes/screens/HomeScreen.js
@@ -1,13 +1,69 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, ScrollView, Image, Text, TextInput } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {BellIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+import Categories from '../components/categories';
+import axios from 'axios'
 
 export default function HomeScreen() {
+
+  const [activeCategory, setActiveCategory] = useState('Beef');
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories();
+  }, [])
+
+  const getCategories = async ()=> {
+    try {
+      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+      if(response && response.data) {
+        setCategories(response.data.categories);
+      }
+
+    } catch(err) {
+      console.log('error: ', err.message)
+    }
+  }
   return (
-    <SafeAreaView className="flex-1 justify-center items-center bg-white">
-      <Text className="text-black text-3xl font-bold">HomeScreen</Text>
+    <SafeAreaView className="flex-1 bg-white">
       <StatusBar style="dark" />
+      <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{paddingBottom: 50}}
+      className="space-y-6 pt-14">
+
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image source={require('../assets/images/avatar.png')} style={{height: hp(5), width: hp(5.5)}}></Image>
+          <BellIcon size={hp(4)} color="gray"></BellIcon>
+        </View>
+
+        <View className="mx-4 space-y-2 mb-2">
+          <Text style={{fontSize: hp(1.7)}} className="text-neutral-600">Hello, Piotr!</Text>
+          <View>
+            <Text style={{fontSize: hp(3.8)}} className="font-semibold text-neutral-600">Make your own food!</Text>
+          </View>
+          <Text style={{fontSize: hp(3.8)}} className="font-semibold text-neutral-600">stay at <Text className="text-sky-300">home</Text></Text>
+        </View>
+
+        <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
+          <TextInput
+          placeholder='Search any recipe'
+          placeholderTextColor={'gray'}
+          style={{fontSize: hp(1.7)}}
+          className="flex-1 text-base mb-1 pl-3 tracking-wider"></TextInput>
+          <View className="bg-white rounded-full p-3">
+            <MagnifyingGlassIcon size={hp(2.5)} strokeWidth={3} color={'gray'}></MagnifyingGlassIcon>
+          </View>
+        </View>
+
+        <View>
+          { categories.length > 0 && <Categories categories={categories} activeCategory={activeCategory} setActiveCategory={setActiveCategory}></Categories>}
+        </View>
+
+      </ScrollView>
     </SafeAreaView>
   )
 }
